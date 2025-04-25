361ab7ead21f2340a0b9cc9dd0d194154ff37b90
zartashbaig <zartashbaig>
2025-04-26 01:51:30 +0500

    changes

--- cyb_workcenter_variant/models/mrp_workcenters.py
+++ cyb_workcenter_variant/models/mrp_workcenters.py
@@ -23,6 +23,21 @@ class MrpWorkcenter(models.Model):
     missing_variant_ids = fields.One2many('workcenter.missing.variant', 'workcenter_id',
                                           string="Missing Product Variants")
 
+    def action_add_variants_to_capacities(self):
+        for rec in self:
+            new_capacity_ids = []
+            if rec.missing_variant_ids:
+                for variant in rec.missing_variant_ids:
+                    new_capacity_ids.append((0, 0, {
+                        'product_id': variant.product_id.id,
+                        'capacity': 200,
+                    }))
+            if new_capacity_ids:
+                rec.write({
+                    'capacity_ids': new_capacity_ids,
+                })
+            rec.missing_variant_ids.unlink()
+
 
 class WorkcenterMissingVariant(models.Model):
     _name = "workcenter.missing.variant"
--- cyb_workcenter_variant/wizard/workcenter_variant_check_wizard.py
+++ cyb_workcenter_variant/wizard/workcenter_variant_check_wizard.py
@@ -13,12 +13,13 @@ class WorkcenterVariantCheckWizard(models.TransientModel):
         workcenter = self.env['mrp.workcenter'].browse(self.env.context.get('active_id'))
 
         existing_variants = workcenter.capacity_ids.mapped('product_id.id')
+        len(existing_variants)
         selected_templates = self.product_template_ids
 
         new_variants = selected_templates.mapped('product_variant_ids').filtered(
             lambda v: v.id not in existing_variants
         )
-
+        len(new_variants)
         workcenter.missing_variant_ids.unlink()
         for variant in new_variants:
             self.env['workcenter.missing.variant'].create({
